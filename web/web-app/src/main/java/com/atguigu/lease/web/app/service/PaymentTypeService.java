4f994a1f91531913ffb0753f23f53d0aab466668
cgx <2227259065@qq.com>
2026-03-19 09:04:10 +0800

    支付方式接口App-根据房间id获取可选支付方式列表

--- web/web-app/src/main/java/com/atguigu/lease/web/app/service/PaymentTypeService.java
+++ web/web-app/src/main/java/com/atguigu/lease/web/app/service/PaymentTypeService.java
@@ -3,10 +3,14 @@ package com.atguigu.lease.web.app.service;
 import com.atguigu.lease.model.entity.PaymentType;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
 * @author liubo
 * @description 针对表【payment_type(支付方式表)】的数据库操作Service
 * @createDate 2023-07-26 11:12:39
 */
 public interface PaymentTypeService extends IService<PaymentType> {
+	//根据房间id获取可选支付方式列表
+	List<PaymentType> getPaymentTypeListByRoomId(Long id);
 }
--- web/web-app/src/main/java/com/atguigu/lease/web/app/service/impl/PaymentTypeServiceImpl.java
+++ web/web-app/src/main/java/com/atguigu/lease/web/app/service/impl/PaymentTypeServiceImpl.java
@@ -4,17 +4,28 @@ import com.atguigu.lease.model.entity.PaymentType;
 import com.atguigu.lease.web.app.mapper.PaymentTypeMapper;
 import com.atguigu.lease.web.app.service.PaymentTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
-* @author liubo
-* @description 针对表【payment_type(支付方式表)】的数据库操作Service实现
-* @createDate 2023-07-26 11:12:39
-*/
+ * @author liubo
+ * @description 针对表【payment_type(支付方式表)】的数据库操作Service实现
+ * @createDate 2023-07-26 11:12:39
+ */
 @Service
 public class PaymentTypeServiceImpl extends ServiceImpl<PaymentTypeMapper, PaymentType>
-    implements PaymentTypeService{
+		implements PaymentTypeService {
 
+	@Autowired
+	private PaymentTypeMapper paymentTypeMapper;
+
+	//根据房间id获取可选支付方式列表
+	@Override
+	public List<PaymentType> getPaymentTypeListByRoomId(Long id) {
+		return paymentTypeMapper.getPaymentTypeListByRoomId(id);
+	}
 }
 
 
